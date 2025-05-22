9ed1a50b54b074e6032c24ed0189b56b2a476bb4
RUB3N02 <rubyerizo@gmail.com>
2025-05-22 10:43:25 +0000

    GENERADOR-DE-EXCUSAS-EN-JAVASCRIPT-RUB3N02

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function() {/* recargar pantalla*/
   //write your code here
   console.log("Hello Rigo from the console!");
+  //crear arrais con frases//
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  //Generar numeros randon//
+  let whoNumber = Math.floor(Math.random()*who.length);/*who*/
+  let actionNumber = Math.floor(Math.random()*action.length);/*action*/
+  let whatNumber =Math.floor(Math.random()*what.length);/*what*/
+  let whenNumber = Math.floor(Math.random()*when.length);/*when*/
+  //document.querySelector()//
+document.querySelector("#excusa").innerHTML= `${who[whoNumber]} ${action[actionNumber]} ${what[whatNumber]} ${when[whenNumber]}.`;
+  
+  
 };
